8fa4dd05a85c49d76d2a236961858c36aadfc79e
Myrotvorets <support@myrotvorets.center>
2021-02-07 20:36:06 +0200

    Update dependencies

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -12,19 +12,12 @@ let environ: Environment | null = null;
 
 export function environment(reset = false): Environment {
     if (!environ || reset) {
-        environ = cleanEnv(
-            process.env,
-            {
-                NODE_ENV: str({ default: 'development' }),
-                PORT: port({ default: 3000 }),
-                IDENTIGRAF_UPLOAD_FOLDER: str(),
-                IDENTIGRAF_MAX_FILE_SIZE: num({ default: 5242880 }),
-            },
-            {
-                strict: true,
-                dotEnvPath: null,
-            },
-        );
+        environ = cleanEnv(process.env, {
+            NODE_ENV: str({ default: 'development' }),
+            PORT: port({ default: 3000 }),
+            IDENTIGRAF_UPLOAD_FOLDER: str(),
+            IDENTIGRAF_MAX_FILE_SIZE: num({ default: 5242880 }),
+        });
     }
 
     return environ;
